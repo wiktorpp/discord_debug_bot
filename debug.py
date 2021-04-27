f43459b3e3e14cf8148e70258897e9836b5ee9f5
wiktorpp <56189344+wiktorpp@users.noreply.github.com>
2021-04-27 12:36:08 +0200

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,8 +1,30 @@
-from libDiscordTools import *
+import discord
+from TOKEN import TOKEN
+from asyncio import *
+import io
 import sys
-from traceback import *
+from datetime import *
 
-async def debug(message):
+client = discord.Client()
+setClient(client)
+
+@client.event
+async def on_ready():
+    print("\033[31mLogged in as {0.user}\033[39m".format(client))
+
+@client.event
+async def on_message(message):
+    globals().update(locals())
+
+    #on_message.msg = message
+    #on_message.message = message
+    #old_stderr = sys.stderr
+    #new_stderr = io.StringIO()
+    #sys.stderr = new_stderr
+
+    if message.content.startswith("$report"):
+        await message.channel.send("Status: online")
+
     if message.content.startswith("%"):
         try:
             old_stdout = sys.stdout
@@ -10,13 +32,13 @@ async def debug(message):
             sys.stdout = new_stdout
 
             progArr = message.content[1:].splitlines()
-            progStrInd = ""
+            progStrIndented = ""
             for i in progArr:
-                progStrInd += "\n    " + i
+                progStrIndented += "\n    " + i
             exec(
-                "async def t():"
-                + progStrInd
-                + "\n    globals().update(locals())",
+                "async def t():" +
+                progStrIndented +
+                "\n    globals().update(locals())",
                 globals()
             )
             await t()
@@ -25,8 +47,15 @@ async def debug(message):
             sys.stdout = old_stdout
 
         except:
-            error = sys.exc_info()
-            debug.error = error
+            error = str(sys.exc_info())
             await message.channel.send(error)
         else:
             await pager(output, message.channel.send)
+
+    globals().update(locals())
+
+    #await message.channel.send(new_stderr.getvalue())
+    #sys.stderr = old_stderr
+
+client.run(TOKEN)
+
