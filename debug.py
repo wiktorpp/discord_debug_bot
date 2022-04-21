60ba02e68633bab9fa2dc80362d842e1f33f54ec
wiktorpp <grazynapp65@gmail.com>
2022-04-21 15:25:57 +0200

    Cleanup

--- debug.py
+++ debug.py
@@ -1,25 +1,23 @@
 import discord
 from TOKEN import TOKEN
-from asyncio import *
+
 import io
 import sys
-from datetime import *
-from pager import pager
 import traceback
+import textwrap
+
+#import ...
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print("\033[31mLogged in as {0.user}\033[39m".format(client))
+    print(f"\033[31mLogged in as {client.user}\033[39m")
 
 @client.event
 async def on_message(message):
     
     #...
-    
-    if message.content.startswith("$report"):
-        await message.channel.send("Status: online")
 
     if message.content.startswith("%"):
         try:
@@ -27,17 +25,14 @@ async def on_message(message):
             new_stdout = io.StringIO()
             sys.stdout = new_stdout
 
-            progArr = message.content[1:].splitlines()
-            progStrIndented = ""
-            for i in progArr:
-                progStrIndented += "\n    " + i
+            request = message.content[1:]
             exec(
-                "async def t():" +
-                progStrIndented +
-                "\n    globals().update(locals())",
+                "async def request_funct():\n" +
+                "    " + request.replace("\n", "\n    ") + "\n"
+                "    globals().update(locals())",
                 globals()
             )
-            await t()
+            await request_funct()
 
             output = new_stdout.getvalue()
             sys.stdout = old_stdout
@@ -46,9 +41,9 @@ async def on_message(message):
             error = traceback.format_exc()
             await message.channel.send(error)
         else:
-            await pager(output, message.channel.send)
-
-    globals().update(locals())
+            import textwrap
+            for chunk in textwrap.wrap(output, 2000, replace_whitespace=False):
+                await message.channel.send(chunk)
 
 client.run(TOKEN)
 
