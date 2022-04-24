2a91464f21c5564d547177520f2aa7c052ead5f0
wiktorpp <grazynapp65@gmail.com>
2022-04-24 16:01:15 +0200

    Fixed bug

--- debug.py
+++ debug.py
@@ -1,11 +1,6 @@
 import discord
 from TOKEN import TOKEN
 
-import io
-import sys
-import traceback
-import textwrap
-
 #import ...
 
 client = discord.Client()
@@ -20,28 +15,43 @@ async def on_message(message):
     #...
 
     if message.content.startswith("%"):
+        import io
+        import sys
+        import traceback
+        import textwrap
         try:
             old_stdout = sys.stdout
             new_stdout = io.StringIO()
             sys.stdout = new_stdout
 
             request = message.content[1:]
-            exec(
-                "async def request_funct():\n" +
-                "    " + request.replace("\n", "\n    ") + "\n"
-                "    globals().update(locals())",
-                globals()
-            )
-            await request_funct()
+            try:
+                exec(
+                    "async def request_funct():\n" +
+                    "    return_value = (" + request + ")" + "\n"
+                    "    globals().update(locals())\n"
+                    "    return return_value",
+                    globals()
+                )
+            except:
+                exec(
+                    "async def request_funct():\n" +
+                    "    " + request.replace("\n", "\n    ") + "\n"
+                    "    globals().update(locals())\n"
+                    "    return None",
+                    globals()
+                )
+            return_value = await request_funct()
 
             output = new_stdout.getvalue()
             sys.stdout = old_stdout
 
+            if return_value != None:
+                output = str(return_value) + "\n" + output
         except:
             error = traceback.format_exc()
             await message.channel.send(error)
         else:
-            import textwrap
             for chunk in textwrap.wrap(output, 2000, replace_whitespace=False):
                 await message.channel.send(chunk)
 
