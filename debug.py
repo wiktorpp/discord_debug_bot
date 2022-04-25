32454de3ee3c9383e6d96a97fd2bec49f5bddef6
wiktorpp <grazynapp65@gmail.com>
2022-04-25 12:12:08 +0200

    Added access to locals

--- debug.py
+++ debug.py
@@ -13,7 +13,6 @@ async def on_ready():
 async def on_message(message):
     
     #...
-
     if message.content.startswith("%"):
         import io
         import sys
@@ -23,32 +22,36 @@ async def on_message(message):
             old_stdout = sys.stdout
             new_stdout = io.StringIO()
             sys.stdout = new_stdout
-
             request = message.content[1:]
+            
             try:
                 exec(
-                    "async def request_funct():\n" +
-                    "    return_value = (" + request + ")" + "\n"
+                    "async def request_funct(other_locals):\n"
+                    "    locals().update(other_locals)\n"
+                    f"    return_value = ({request})\n"
                     "    globals().update(locals())\n"
-                    "    return return_value",
-                    globals()
+                    "    return return_value\n"
+                    "globals().update(locals())\n"
                 )
             except:
+                request = request.replace('\n', '\n    ')
                 exec(
-                    "async def request_funct():\n" +
-                    "    " + request.replace("\n", "\n    ") + "\n"
+                    "async def request_funct(other_locals):\n"
+                    "    locals().update(other_locals)\n"
+                    f"    {request}\n"
                     "    globals().update(locals())\n"
-                    "    return None",
-                    globals()
+                    "    return return_value\n"
+                    "globals().update(locals())\n"
                 )
-            return_value = await request_funct()
+            return_value = await request_funct(locals())
 
             output = new_stdout.getvalue()
             sys.stdout = old_stdout
 
             if return_value != None:
                 output = str(return_value) + "\n" + output
-        except:
+        except KeyError:
+
             error = traceback.format_exc()
             await message.channel.send(error)
         else:
