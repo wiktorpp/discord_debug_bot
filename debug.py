243ee7798c349f22557cd2bb094524ad794e265d
wiktorpp <grazynapp65@gmail.com>
2022-11-07 07:43:06 +0100

    Move debug code to a seperate file

--- debug.py
+++ debug.py
@@ -1,65 +1,48 @@
+import io
+import sys
+import traceback
 import discord
-from TOKEN import TOKEN
 
-#import ...
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print(f"\033[31mLogged in as {client.user}\033[39m")
-
-@client.event
-async def on_message(message):
-    
-    #...
-
-    if message.content.startswith("%"):
-        import io
-        import sys
-        import traceback
+async def run_message(message, other_locals):
+    try:
+        old_stdout = sys.stdout
+        new_stdout = io.StringIO()
+        sys.stdout = new_stdout
+        request_str = message.content[1:]
+        
         try:
-            old_stdout = sys.stdout
-            new_stdout = io.StringIO()
-            sys.stdout = new_stdout
-            request = message.content[1:]
-            
-            try:
-                exec(
-                    "async def request_funct(other_locals):\n"
-                    "    locals().update(other_locals)\n"
-                    f"    return_value = ({request})\n"
-                    "    globals().update(locals())\n"
-                    "    return return_value\n"
-                    "globals().update(locals())\n"
-                )
-            except:
-                request = request.replace('\n', '\n    ')
-                exec(
-                    "async def request_funct(other_locals):\n"
-                    "    locals().update(other_locals)\n"
-                    f"    {request}\n"
-                    "    globals().update(locals())\n"
-                    "globals().update(locals())\n"
-                )
-            return_value = await request_funct(locals())
-
-            output = new_stdout.getvalue()
-            sys.stdout = old_stdout
-
-            if return_value != None:
-                output = str(return_value) + "\n" + output
+            exec(
+                "async def request_funct(other_locals):\n"
+                "    locals().update(other_locals)\n"
+                f"    return_value = ({request_str})\n"
+                "    globals().update(locals())\n"
+                "    return return_value\n"
+                "globals().update(locals())\n"
+            )
         except:
-            error = traceback.format_exc()
-            await message.channel.send(f"```\n{error}\n```")
+            request = request.replace('\n', '\n    ')
+            exec(
+                "async def request_funct(other_locals):\n"
+                "    locals().update(other_locals)\n"
+                f"    {request_str}\n"
+                "    globals().update(locals())\n"
+                "globals().update(locals())\n"
+            )
+        return_value = await request_funct(locals())
+
+        output = new_stdout.getvalue()
+        sys.stdout = old_stdout
+
+        if return_value != None:
+            output = str(return_value) + "\n" + output
+    except Exception:
+        error = traceback.format_exc()
+        await message.channel.send(f"```\n{error}\n```")
+    else:
+        if not len(output) > 2000:
+            await message.channel.send(output)
         else:
-            if not len(output) > 2000:
-                await message.channel.send(output)
-            else:
-                await message.channel.send(
-                    file=discord.File(io.BytesIO(output.encode()), 
-                    filename="output.txt")
-                )
-
-client.run(TOKEN)   
-
+            await message.channel.send(
+                file=discord.File(io.BytesIO(output.encode()), 
+                filename="output.txt")
+            )
---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+from TOKEN import TOKEN
+import debug
+
+#import ...
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f"\033[31mLogged in as {client.user}\033[39m")
+
+#@client.event
+#async def ...
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    #...
+
+    if message.content.startswith("%"):
+        await debug.run_message(message, locals())
+
+client.run(TOKEN)   
+
